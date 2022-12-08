bfe7c4dc8a99a011486663e99f2321db19557b7e
Jonathan Short <101197371+4shorts@users.noreply.github.com>
2022-12-08 17:24:10 -0500

    Move AI

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class AI : MonoBehaviour
 {
+    
     [SerializeField]
     private List<Transform> _wayPoints;
     private NavMeshAgent _agent;
     private int _currentPoint = 0;
-    private bool _inReverse = false;
+    [SerializeField]
+    
 
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
 
+        
+
         if (_agent != null)
         {
-            _agent.destination = _wayPoints[_currentPoint].position;
+            _agent.destination = _wayPoints[1].position;
         }
         
     }
@@ -27,49 +32,9 @@ public class AI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CalculateAIMovement();
+
+        
     }
 
-    void CalculateAIMovement()
-    {
-        if (_agent.remainingDistance < 0.5f)
-        {
-            if (_inReverse == true)
-            {
-                Reverse();
-            }
-            else
-            {
-                Forward();
-            }
-            _agent.SetDestination(_wayPoints[_currentPoint].position);
-
-        }
-
-        void Forward()
-        {
-            if (_currentPoint == _wayPoints.Count - 1)
-            {
-                _inReverse = true;
-                _currentPoint--;
-            }
-
-            else
-            {
-                _currentPoint++;
-            }
-        }
-        void Reverse()
-        {
-            if (_currentPoint == 0)
-            {
-                _inReverse = false;
-                _currentPoint++;
-            }
-            else
-            {
-                _currentPoint--;
-            }
-        }
-    }
+    
 }
